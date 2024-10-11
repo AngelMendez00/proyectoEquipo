765a76ccb82dd463b1d95e248e5b608e04338c7e
AngelMendez <angelmen1028@gmail.com>
2024-10-11 02:43:28 -0600

    modificacion

--- guia/guia.py
+++ guia/guia.py
@@ -1,2 +1,15 @@
-class Guia:
-    pass
+from empleados.empleado import Empleado
+from datetime import datetime
+from empleados.utils.rol import Rol
+
+class Guia(Empleado):
+    
+    def __init__(self, nombre: str, 
+                apellidos: str, 
+                fecha_nacimiento: datetime, 
+                fecha_inicio: datetime, 
+                rfc: str, 
+                curp: str, 
+                salario: float, 
+                horario: datetime):
+        super().__init__(nombre=nombre, apellidos=apellidos, fecha_nacimiento=fecha_nacimiento, fecha_inicio=fecha_inicio, rfc=rfc, curp=curp, salario=salario, horario=horario, rol=Rol.GUIA)
--- mantenimiento/mantenimiento.py
+++ mantenimiento/mantenimiento.py
@@ -1,2 +1,16 @@
-class Mantenimiento:
-    pass
+from empleados.empleado import Empleado
+from datetime import datetime
+from empleados.utils.rol import Rol
+
+class Mantenimiento(Empleado):
+    
+
+    def __init__(self, nombre: str, 
+                apellidos: str, 
+                fecha_nacimiento: datetime, 
+                fecha_inicio: datetime, 
+                rfc: str, 
+                curp: str, 
+                salario: float, 
+                horario: datetime):
+        super().__init__(nombre=nombre, apellidos=apellidos, fecha_nacimiento=fecha_nacimiento, fecha_inicio=fecha_inicio, rfc=rfc, curp=curp, salario=salario, horario=horario, rol=Rol.MANTENIMIENTO)
